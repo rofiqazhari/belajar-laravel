a9583ca97049db4ad540ad1212414f4a36ddefe6
ROFIQ AZHARI <rofiqazhari@gmail.com>
2020-08-05 21:43:17 +0700

    revisi WELCOME

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -6,7 +6,7 @@
     <title>Master Barang</title>
 </head>
 <body>
-    <a href="/laravel1">HALAMAN UTAMA</a>
+    <a href="/laravel1">HALAMAN DEPAN</a>
     <h1>Nama Barang : {{ $nmbarang }} <br> Jenis : {{ $jnbarang }}</h1>    
 </body>
 </html>
