43d6832cb16d2bcf5db4abe4f049f146281c664e
ROFIQ AZHARI <rofiqazhari@gmail.com>
2020-08-05 21:36:03 +0700

    revide welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,8 @@
                 <div class="links">
                     <a href="login">Login</a>
                     <a href="barang">Barang</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="kategori">Kategori</a>
+                    <a href="jenis">Jenis</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
